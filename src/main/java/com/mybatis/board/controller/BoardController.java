2d961bb31c89f3d2b68b2db00ec04aa6132fb359
hongui <honge7694@naver.com>
2024-03-30 01:46:46 +0900

    feature: 게시글 작성 데이터 컨트롤러에 가져오기

--- src/main/java/com/mybatis/board/controller/BoardController.java
+++ src/main/java/com/mybatis/board/controller/BoardController.java
@@ -1,8 +1,11 @@
 package com.mybatis.board.controller;
 
+import com.mybatis.board.dto.BoardDto;
 import com.mybatis.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,4 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class BoardController {
     private final BoardService boardService;
+
+    @GetMapping("/save")
+    public String save() {
+        return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(BoardDto boardDto) {
+        System.out.println("boardDto = " + boardDto);
+        return "index";
+    }
 }
